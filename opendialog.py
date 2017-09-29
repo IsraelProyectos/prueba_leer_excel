b9f27e5f968b05e1c9f81998124f024f5e21e221
IsraelPuyol <israelpuyol@hotmail.com>
2017-09-29 23:32:24 +0200

    funcionando

--- opendialog.py
+++ opendialog.py
@@ -5,19 +5,44 @@ import openpyxl
 class MyFrame(wx.Frame):
             def __init__(self, parent, title):
                   wx.Frame.__init__(self, parent, title=title, size=(500,200))
-
-                  # doc.get_sheet_names()
-                  # ['sheet1']
-                  doc = openpyxl.load_workbook('prueba.xlsx') #suponiendo que el archivo esta en el mismo directorio del script
-                  hoja = doc.get_sheet_by_name('Sheet1')
-                  for fila in hoja.rows:
-                        for columna in fila:
-                              print columna.coordinate, columna.value
-
+                  self.columna_excel = [ ]
+                  self.todas_columnas = [ ]
+                  self.registro_excel_final = [ ]
+                  self.registros_excel_final =[ ]
+                  self.email = ""
+                  self.doc = openpyxl.load_workbook('C:\Users\Israel\Desktop\prueba.xlsx') #suponiendo que el archivo esta en el mismo directorio del script
+                  self.hoja = self.doc.get_sheet_by_name('Ventas AO y AOA')
+                  self.i = 0
                   self.txtRuta = wx.TextCtrl(self, pos=(80,50), size=(150,20))
                   self.buttonTextArea = wx.Button(self, label="Open", pos=(260,50), size=(100,20))
-                  self.buttonTextArea.Bind(wx.EVT_BUTTON, self.OnbuttonTextArea)
+                  self.buttonTextArea.Bind(wx.EVT_BUTTON, self.createExcel)
                   self.Show(True)
+
+            def createExcel(self, e):
+
+                  for fila in self.hoja.rows:
+                  	#if i != 1:
+					for columna in fila:
+						self.columna_excel.append(columna.value)
+					
+					if self.i !=0:
+						self.todas_columnas.append(self.columna_excel[0:-1])
+					self.columna_excel = [ ]
+					self.i +=1
+                 
+                  for fila in self.todas_columnas:
+                  		if self.email == fila[2]:
+                  			self.registros_excel_final[-1][0] = self.registros_excel_final[-1][0] + "; " + fila[0]
+                  			self.registros_excel_final[-1][3] = str(self.registros_excel_final[-1][3]) + "; " + str(fila[3])
+                  			self.registros_excel_final[-1][4] = str(self.registros_excel_final[-1][4]) + "; " + str(fila[4])
+                  			#print(fila[3])
+              			else:
+              				self.registro_excel_final.append(fila)
+              				self.registros_excel_final.append(self.registro_excel_final[-1])
+              				self.registro_excel_final = [ ]
+              			self.email = fila[2]
+
+                  print(self.registros_excel_final)
 
             
 app = wx.App(False)
