2d193976d69ddc6f0b3ddc3d4d2fe00e01ac98fd
IsraelPuyol <israelpuyol@hotmail.com>
2017-10-01 01:31:12 +0200

    openfiledialog

--- opendialog.py
+++ opendialog.py
@@ -7,24 +7,31 @@ from openpyxl.styles import Color, PatternFill, Font, Border
 class MyFrame(wx.Frame):
             def __init__(self, parent, title):
                   wx.Frame.__init__(self, parent, title=title, size=(500,200))
+
+                  self.pathFile = ''
+                  self.txtRuta = wx.TextCtrl(self, pos=(80,50), size=(150,20))
+                  self.buttonFind = wx.Button(self, label="Buscar...", pos=(260,50), size=(100,20))
+                  self.buttonFind.Bind(wx.EVT_BUTTON, self.openFile)
+                  self.buttonExecute = wx.Button(self, label="Convertir", pos=(260,100), size=(100,20))
+                  self.buttonExecute.Bind(wx.EVT_BUTTON, self.createExcel)
+
                   self.columna_excel = [ ]
                   self.todas_columnas = [ ]
                   self.registro_excel_final = [ ]
                   self.registros_excel_final =[ ]
                   self.email = ""
-                  self.doc = load_workbook('C:\Users\Israel\Desktop\prueba.xlsx')
-                  self.hoja = self.doc.get_sheet_by_name('Ventas AO y AOA')
                   self.i = 0
                   self.z = 1
-                  self.txtRuta = wx.TextCtrl(self, pos=(80,50), size=(150,20))
-                  self.buttonTextArea = wx.Button(self, label="Open", pos=(260,50), size=(100,20))
-                  self.buttonTextArea.Bind(wx.EVT_BUTTON, self.createExcel)
                   self.Show(True)
 
             def createExcel(self, e):
 
+            	  #Cargando fichero desde textBox, obtenido de openFileDialog
+            	  doc = load_workbook(self.pathFile)
+                  hoja = doc.get_sheet_by_name('Ventas AO y AOA')
+
             	  #Leyendo filas del excel y guardandola en una lista
-                  for fila in self.hoja.rows:
+                  for fila in hoja.rows:
 					for columna in fila:
 						self.columna_excel.append(columna.value)
 					
@@ -74,7 +81,24 @@ class MyFrame(wx.Frame):
                   
                   
                   book.save('regalos_concesionarios.xlsx')
-               
+            
+            def openFile(self, e):
+
+            	with wx.FileDialog(self, "Open XLSX file", wildcard="XLSX files (*.xlsx)|*.xlsx",
+                       style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
+
+		        if fileDialog.ShowModal() == wx.ID_CANCEL:
+		            return     # the user changed their mind
+
+		        # Proceed loading the file chosen by the user
+		        self.pathFile = fileDialog.GetPath()
+		        self.txtRuta.SetValue(self.pathFile)
+		        print(self.pathFile)
+		        # try:
+		        #     with open(pathname, 'r') as file:
+		        #         self.doLoadDataOrWhatever(file)
+		        # except IOError:
+		        #     wx.LogError("Cannot open file '%s'." % newfile)   
 
 #Inicio de la aplicacion           
 app = wx.App(False)
